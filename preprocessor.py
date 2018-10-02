37ddf390a3ff28a57dd5a0cca38bbe563aa9ed1a
pgogousis <pgogousis@gmail.com>
2018-10-03 00:21:10 +0100

    Preprocessor is working

--- preprocessor.py
+++ preprocessor.py
@@ -1,29 +1,75 @@
+import nltk
+import re
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
+import time
+import json
+import collections
+import csv
 
+toc = time.time()
 
-filepath = 'bible.txt'
+# Check whether a term is  within a my term dictionary or not
+def AddToTermList(key):
+    if key in termList:
+        termList[key] = termList[key]+1
+    else:
+        termList[key] = 1
 
-ps = PorterStemmer()
+# Function that checks whether the word is a stopword
+def isNotAStopword(string):
+    if string not in stopwords:
+        return True
 
-termList = []
+# Function that formats a string - lower case, removes special characters
+def StringFormatting(string):
+    stringLowerCase = string.lower()
+    stringFormatted = re.sub('[^A-Za-z0-9]+', '', stringLowerCase) # If char is not alphanumeric eliminate
+    return stringFormatted
 
-f = open("output.txt", "a")
+# Just removes spaces
+def Tokenizer(line):
+    spaceRemoved = word_tokenize(line)
 
-with open('stopwords.txt') as stopWordFile:
-    stopwords = stopWordFile.read().splitlines()
+# Function that imports the stopwords list
+def ImportStopwordList(pathToFile):
+    with open(pathToFile) as stopWordFile:
+        stopwords = stopWordFile.read().splitlines()
 
-with open(filepath) as bible:
-    for counter, line in enumerate(bible):
+#filepath = '/data/bible.txt'
+stopwordPath = 'stopwords.txt'
+stopwords =[] # List of stopwords
+
+ImportStopwordList(stopwordPath)
+
+ps = PorterStemmer() # PorterStemmer toolkit
+termList = {} # This is my inverted index - Should implement it with a file! Must NOT cache list?
+
+f = open("output.txt", "w")
+
+with open('data/bible.txt') as currentFile:
+    for counter, line in enumerate(currentFile):
         for term in word_tokenize(line):
-            termLowCase = term.lower()
-            if termLowCase not in stopwords:
-                termList.append(ps.stem(termLowCase))
-                f.write(ps.stem(termLowCase))
+            termFormatted = StringFormatting(term);
+            termStemmed = ps.stem(termFormatted)
+            f.write(ps.stem(termStemmed)+'\n')
+            if isNotAStopword(termStemmed):
+                AddToTermList(termStemmed)
+
+    f.close()
 
 '''
-
-with open('output.txt', 'w') as f:
-    for item in sortedList:
-        print >> thefile, item
+    od = collections.OrderedDict(sorted(termList.items()))
+    json = json.dumps(od)
+    f = open("dict.json","w")
+    f.write(json)
+    f.close()
 '''
+
+with open('test.csv', 'w') as f:
+    for key in termList.keys():
+        f.write("%s,%s\n"%(key,termList[key]))
+
+tic = time.time()
+
+print("Total runtime = {}".format(tic-toc))
